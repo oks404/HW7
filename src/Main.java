a044c1994ba07862483401cd8b2d5f8851a189e8
oks404 <ksunya2004@list.ru>
2022-11-26 21:22:56 +1000

    Задания 1.3 -2.4

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-//  Задание 1
+//  Задание 1.1
         int salary = 15_000;
         int total = 0;
         int month = 0;
-        while (total <= 2_459_000){
-            total = total + total/100;
+        while (total <= 2_459_000) {
+            total = total + total / 100;
             total = total + salary;
             month = month + 1;
+            if (total >= 2_459_000) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+            }
         }
-        if (total >= 2_459_000){
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
-        }
-//  Задание 2
+//  Задание 1.2
         int a = 0;
         while (a < 10){
             a = a + 1;
@@ -22,7 +24,7 @@ public class Main {
         for (int b = 10; b >= 1; b--){
             System.out.print(b + " ");
         }
-//  Задание 3
+//  Задание 1.3
         int populationY = 12_000_000;
         int birthRateInYear = 17 * (populationY / 1_000);
         int deathRateInYear = 8 * (populationY / 1_000);
@@ -32,5 +34,22 @@ public class Main {
             totalPopulation = totalPopulation + increaceInYear;
             System.out.print("\n Год " + i + ", численность населения составляет " + (totalPopulation + populationY));
         }
+//  Задание 2.1 - 2.3
+        int vasyaSaveMoneyinMonth = 15_000;
+        int totalVasyaSave = 0;
+        int monthVasya = 0;
+//        while (totalVasyaSave <= 12_000_000){    Задание 2.1 - 2.2
+        while (monthVasya <= 9 * 12){
+            totalVasyaSave = totalVasyaSave + (totalVasyaSave / 100 * 7);
+            totalVasyaSave = totalVasyaSave + vasyaSaveMoneyinMonth;
+            monthVasya++;
+            if ( monthVasya % 6 == 0){
+                System.out.print("\n Месяц " + monthVasya + ", сумма накоплений равна " + totalVasyaSave + " рублей");
+            }
+        }
+//  Задание 2.4
+        int firstFriday = 5;
+        for (; firstFriday <= 31; firstFriday = firstFriday + 7)
+            System.out.print("\n Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");
+        }
     }
-}
